69ff8b7986f3f4e8bade9a179d0032757f3ba206
ainur270707 <103682607+ainur270707@users.noreply.github.com>
2024-10-30 16:11:15 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,16 +1,27 @@
 import socket
+import threading
+import json
 
-HOST = '0.0.0.0'  # Слушать на всех интерфейсах
-PORT = 12345
+# Настройка сервера
+HOST = '0.0.0.0'  # Указываем 0.0.0.0, чтобы сервер слушал на всех интерфейсах
+PORT = 52128       # Порт сервера
 
+# Создаем серверный сокет
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
-server_socket.listen()
+server_socket.listen(5)
 
-print("Server is listening...")
+print("Сервер запущен. Хост:", socket.gethostname(), "Порт:", PORT)
+
+def handle_client(client_socket):
+    # Обработка клиента
+    request = client_socket.recv(1024).decode('utf-8')
+    print("Получен запрос:", request)
+    client_socket.send("Ответ от сервера".encode('utf-8'))
+    client_socket.close()
 
 while True:
-    conn, addr = server_socket.accept()
-    print(f"Connected by {addr}")
-    conn.sendall(b"Welcome to the server")
-    conn.close()
+    client_socket, addr = server_socket.accept()
+    print("Подключен клиент:", addr)
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
